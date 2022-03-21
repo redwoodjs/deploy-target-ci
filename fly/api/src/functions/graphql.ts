d5695eebccf64a848d7dda97ea378ea75476d82f
Rob Cameron <rob.cameron@fastmail.com>
2022-03-20 19:41:10 -0700

    Gotta have a specific domain for CORS when including credentials

--- fly/api/src/functions/graphql.ts
+++ fly/api/src/functions/graphql.ts
@@ -15,6 +15,10 @@ export const handler = createGraphQLHandler({
   directives,
   sdls,
   services,
+  cors: {
+    origin: 'https://d2dub2qmgsm5yj.cloudfront.net',
+    credentials: true,
+  },
   onException: () => {
     // Disconnect from your database with an unhandled exception.
     db.$disconnect()
