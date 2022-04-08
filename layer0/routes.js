765f2552a5d0e445eb7c5d01f05cfac0e11121f0
Tristan Lee <tristanlee85@gmail.com>
2022-04-08 14:33:43 -0400

    Add graphql caching to Layer0 router (#180)
    Co-authored-by: David Price <thedavid@thedavidprice.com>

--- layer0/routes.js
+++ layer0/routes.js
@@ -3,4 +3,21 @@
 import { Router } from '@layer0/core'
 import { redwoodRoutes } from '@layer0/redwood'
 
-export default new Router().use(redwoodRoutes)
+const apiPath = '/.redwood/functions/graphql'
+
+export default new Router()
+  .graphqlOperation(
+    {
+      path: apiPath,
+      name: 'BlogPostsQuery',
+    },
+    ({ cache }) => {
+      cache({
+        edge: {
+          maxAgeSeconds: 60 * 60, // cache responses for one hour
+          staleWhileRevalidateSeconds: 60 * 60 * 24, // serve stale responses for up to 24 hours
+        },
+      })
+    }
+  )
+  .use(redwoodRoutes)
