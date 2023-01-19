eaff7af73dd7dfa3766c569f739b88db1d9e1ad2
Dominic Saadi <dominiceliassaadi@gmail.com>
2023-01-19 14:26:19 -0800

    debug: add origins

--- vercel/api/src/functions/graphql.ts
+++ vercel/api/src/functions/graphql.ts
@@ -16,6 +16,9 @@ export const handler = createGraphQLHandler({
     logger,
     options: { operationName: true, query: true, requestId: true },
   },
+  cors: {
+    origin: ['https://bazinga.kittens', 'https://www.kittens.bazinga'],
+  },
   directives,
   sdls,
   services,
