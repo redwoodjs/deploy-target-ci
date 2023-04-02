8a7ab113b1858e3b355ffcfd26c9c0d655d90d03
David Price <thedavid@thedavidprice.com>
2023-04-02 00:16:40 -0700

    Create Heroku and Coherence Project (#786)
    * add heroku, coherence; remove fly
    
    * update heroku
    
    * update coherence

---
+++ coherance/api/src/services/users/users.scenarios.ts
@@ -0,0 +1,26 @@
+import type { Prisma, User } from '@prisma/client'
+
+import type { ScenarioData } from '@redwoodjs/testing/api'
+
+export const standard = defineScenario<Prisma.UserCreateArgs>({
+  user: {
+    one: {
+      data: {
+        email: 'String2636015',
+        hashedPassword: 'String',
+        fullName: 'String',
+        salt: 'String',
+      },
+    },
+    two: {
+      data: {
+        email: 'String2652135',
+        hashedPassword: 'String',
+        fullName: 'String',
+        salt: 'String',
+      },
+    },
+  },
+})
+
+export type StandardScenario = ScenarioData<User, 'user'>
