1e2f20398de1cd9b58a6ef620fa23280aeec5d5c
David Price <thedavid@thedavidprice.com>
2023-04-01 23:53:56 -0700

    add heroku, coherence; remove fly

---
+++ coherance/api/src/services/posts/posts.scenarios.ts
@@ -0,0 +1,38 @@
+import type { Prisma, Post } from '@prisma/client'
+
+import type { ScenarioData } from '@redwoodjs/testing/api'
+
+export const standard = defineScenario<Prisma.PostCreateArgs>({
+  post: {
+    one: {
+      data: {
+        title: 'String',
+        body: 'String',
+        author: {
+          create: {
+            email: 'String5425458',
+            hashedPassword: 'String',
+            fullName: 'String',
+            salt: 'String',
+          },
+        },
+      },
+    },
+    two: {
+      data: {
+        title: 'String',
+        body: 'String',
+        author: {
+          create: {
+            email: 'String9772564',
+            hashedPassword: 'String',
+            fullName: 'String',
+            salt: 'String',
+          },
+        },
+      },
+    },
+  },
+})
+
+export type StandardScenario = ScenarioData<Post, 'post'>
