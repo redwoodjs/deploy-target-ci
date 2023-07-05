9fd482a41995d6a27eb7e401c6805dd2f1a211b9
Dominic Saadi <dominiceliassaadi@gmail.com>
2023-07-05 11:48:29 -0700

    heroku

--- heroku/web/src/pages/WaterfallPage/WaterfallPage.stories.tsx
+++ heroku/web/src/pages/WaterfallPage/WaterfallPage.stories.tsx
@@ -1,12 +1,14 @@
-import type { ComponentMeta, ComponentStory } from '@storybook/react'
+import type { StoryObj, Meta } from '@storybook/react'
 
 import WaterfallPage from './WaterfallPage'
 
-export const generated: ComponentStory<typeof WaterfallPage> = (args) => {
-  return <WaterfallPage id={42} {...args} />
+export const generated: StoryObj<typeof WaterfallPage> = {
+  render: (args) => {
+    return <WaterfallPage id={42} {...args} />
+  },
 }
 
 export default {
   title: 'Pages/WaterfallPage',
   component: WaterfallPage,
-} as ComponentMeta<typeof WaterfallPage>
+} as Meta<typeof WaterfallPage>
