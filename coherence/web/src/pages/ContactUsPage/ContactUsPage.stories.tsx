bec8034f0e34b9bbfe9c4bd37e66a30e9a730592
Dominic Saadi <dominiceliassaadi@gmail.com>
2023-07-05 11:32:53 -0700

    coherence

--- coherence/web/src/pages/ContactUsPage/ContactUsPage.stories.tsx
+++ coherence/web/src/pages/ContactUsPage/ContactUsPage.stories.tsx
@@ -1,4 +1,4 @@
-import type { ComponentMeta } from '@storybook/react'
+import type { Meta } from '@storybook/react'
 
 import ContactUsPage from './ContactUsPage'
 
@@ -9,4 +9,4 @@ export const generated = () => {
 export default {
   title: 'Pages/ContactUsPage',
   component: ContactUsPage,
-} as ComponentMeta<typeof ContactUsPage>
+} as Meta<typeof ContactUsPage>
