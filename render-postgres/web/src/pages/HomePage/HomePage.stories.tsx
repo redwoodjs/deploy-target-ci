c594b7fa7900e9f4f950d50930ef7a68d4f048ed
Dominic Saadi <dominiceliassaadi@gmail.com>
2023-07-05 12:13:05 -0700

    render

--- render-postgres/web/src/pages/HomePage/HomePage.stories.tsx
+++ render-postgres/web/src/pages/HomePage/HomePage.stories.tsx
@@ -1,4 +1,4 @@
-import type { ComponentMeta } from '@storybook/react'
+import type { Meta } from '@storybook/react'
 
 import HomePage from './HomePage'
 
@@ -9,4 +9,4 @@ export const generated = () => {
 export default {
   title: 'Pages/HomePage',
   component: HomePage,
-} as ComponentMeta<typeof HomePage>
+} as Meta<typeof HomePage>
