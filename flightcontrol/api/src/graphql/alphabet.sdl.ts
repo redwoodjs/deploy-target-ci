2bbe9040b658d102617ad2756e969264a1a37dad
Dominic Saadi <dominiceliassaadi@gmail.com>
2024-02-23 02:42:00 -0800

    Revert flightcontrol changes
    This reverts commit 873ad32957e39b51d0ffc9f02fd05da8d395854f.

--- flightcontrol/api/src/graphql/alphabet.sdl.ts
+++
@@ -1,9 +0,0 @@
-export const schema = gql`
-  type Query {
-    """
-    A field that spells out the letters of the alphabet
-    Maybe you want to @stream this field ;)
-    """
-    alphabet: [String!]! @skipAuth
-  }
-`
